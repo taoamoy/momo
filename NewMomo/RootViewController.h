46b700da55177dea5f7104cae242fbca049dd3fb
taohong <taohong@qq.com>
2016-10-06 10:06:38 +0800

    创建了rootViewCtr

---
+++ NewMomo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  NewMomo
+//
+//  Created by etcxm on 16/10/6.
+//  Copyright © 2016年 etcxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
